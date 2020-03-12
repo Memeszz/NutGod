31e0a39c065f1efca8a89875dc906235ca38c960
Wks JanStudio <37088203+SirRina@users.noreply.github.com>
2020-03-12 20:09:45 -0300

    FovSlider effic, sorry meme!

--- src/main/java/me/zeroeightsix/kami/module/modules/render/FovSlider.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/render/FovSlider.java
@@ -7,12 +7,24 @@ import me.zeroeightsix.kami.setting.Settings;
 /**
  * Memeszz insane module
  */
+
+// Rina updateee!
 @Module.Info(name = "FovSlider", description = "Fov Slider", category = Module.Category.RENDER)
 public class FovSlider extends Module {
+    private Setting<Integer> fov = register(Settings.integerBuilder("Fov").withMinimum(0).withValue(150).withMaximum(179).build());
 
-    private Setting<Integer> fov = register(Settings.integerBuilder("Fov").withMinimum(0).withValue(150).withMaximum(180).build());
+    public float old_fov = mc.gameSettings.fovSetting;
+    public float new_fov;
 
-    public void onUpdate(){
-        mc.gameSettings.fovSetting = (float)fov.getValue();
+    @Override
+    public void onDisable() {
+    	mc.gameSettings.fovSetting; = old_fov;
     }
+
+    @Override
+    public void onUpdate(){
+    	new_fov = (float) fov.getValue();
+
+        mc.gameSettings.fovSetting = new_fov
+	}
 }
