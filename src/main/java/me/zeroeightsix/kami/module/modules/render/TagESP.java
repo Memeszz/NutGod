beff3a5ad3c46e09081ae76998a9373d9cf14dc8
Wks JanStudio <37088203+SirRina@users.noreply.github.com>
2020-03-12 20:33:13 -0300

    I have a tick about namespaces;

--- src/main/java/me/zeroeightsix/kami/module/modules/render/TagESP.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/render/TagESP.java
@@ -1,10 +1,16 @@
 package me.zeroeightsix.kami.module.modules.render;
 
-import me.zeroeightsix.kami.module.Module;
-import me.zeroeightsix.kami.setting.Setting;
 import me.zeroeightsix.kami.setting.Settings;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.module.Module;
 
+// Rina.
 @Module.Info(name = "TagESP", category = Module.Category.RENDER);
 public class TagESP extends Module {
-	private Setting<Boolean> show_durability = register(Settings.b("Durability", false));
+	private Setting<Boolean> durabilidade = register(Settings.b("Durability", false));
+
+	@Override
+	public void onUpdate() {
+
+	}
 }
